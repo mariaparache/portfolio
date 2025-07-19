0f5ed96aa8820d34cf3c0388c5d137420dbae638
Maria Parache <114952062+mariaparache@users.noreply.github.com>
2025-07-19 18:01:54 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// For the images carousel in 'About Me' section
+const images = document.querySelectorAll('.carousel-img');
+  let current = 0;
+
+  function showImage(index) {
+    images.forEach((img, i) => {
+      img.classList.toggle('active', i === index);
+    });
+  }
+
+  function nextImage() {
+    current = (current + 1) % images.length;
+    showImage(current);
+  }
+
+  function prevImage() {
+    current = (current - 1 + images.length) % images.length;
+    showImage(current);
+  }
